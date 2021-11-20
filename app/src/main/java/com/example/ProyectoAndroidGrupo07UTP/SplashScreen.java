950d48a92807de4757d6eedf28991e95e1090a5c
Ana Cecilia Vélez Vinasco <anaceciliav@protonmail.com>
2021-11-20 11:18:39 -0500

    Login

--- app/src/main/java/com/example/ProyectoAndroidGrupo07UTP/SplashScreen.java
+++ app/src/main/java/com/example/ProyectoAndroidGrupo07UTP/SplashScreen.java
@@ -32,7 +32,7 @@ public class SplashScreen extends AppCompatActivity {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run(){
-                Intent newIntent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent newIntent = new Intent(getApplicationContext(),Login.class);
                 startActivity(newIntent);
                 finish();
 
